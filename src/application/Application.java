4aa473d8191794e87b76dbe3f82e153ba2343330
Carolina <Carolina@Carol>
2014-11-05 12:40:48 +0000

    MoneyCalculator

--- src/application/Application.java
+++ src/application/Application.java
@@ -1,8 +1,12 @@
 package application;
 
+import model.CurrencySet;
+import persistence.CurrencySetLoader;
+
 public class Application {
 
     public static void main(String[] args) {
+        CurrencySet set = new CurrencySetLoader().load();
     }
     
 }
--- src/persistence/CurrencySetLoader.java
+++ src/persistence/CurrencySetLoader.java
@@ -1,5 +1,12 @@
 package persistence;
 
+import model.CurrencySet;
+
 public class CurrencySetLoader {
+    
+    public CurrencySet load() {
+        CurrencySet set = new CurrencySet();
+        return set;
+    }
     
 }
